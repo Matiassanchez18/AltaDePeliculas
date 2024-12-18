b4e7cf77541fc10621a4f31ca92e88e697df8468
Matias <matiassanchez13422@gmail.com>
2024-12-17 23:10:07 -0300

    Se termino de arreglar las insersiones de cards

--- src/components/ListaPeliculas.jsx
+++ src/components/ListaPeliculas.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import Peliculas from'./Peliculas'
+import React from "react";
+import Peliculas from "./Peliculas";
 
-const ListaPeliculas = ({arrayPeliculas}) => {
-    return (
-        <section className='mt-5  p-3'>
-            <article className='border'>
-            <div className="bg-white">
+const ListaPeliculas = ({ arrayPeliculas }) => {
+  return (
+    <section className="mt-5  p-3">
+      <article className="border fondo shadow border-start border-end">
+        <div className="bg-white">
           <h2 className="text-center  p-2">Peliculas agregadas</h2>
         </div>
-                {
-                    arrayPeliculas.map((pelicula, posicion)=>(<Peliculas pelicula = {pelicula} key ={posicion}  ></Peliculas>))
-                }
-                
-            </article>
-        </section>
-    );
+        <div className=" row justify-content-center">
+          {arrayPeliculas.map((pelicula, posicion) => (
+            <Peliculas pelicula={pelicula} key={posicion}></Peliculas>
+          ))}
+        </div>
+      </article>
+    </section>
+  );
 };
 
 export default ListaPeliculas;
--- src/components/Peliculas.jsx
+++ src/components/Peliculas.jsx
@@ -4,50 +4,24 @@ import "../css/style.css"; // Si tienes estilos personalizados
 
 const PeliculasAgregadas = () => {
   return (
-    <section>
-    
-      <article className="fondo shadow border-start border-end" >
-        <div className=" row justify-content-center">
-          <div className="col-12 col-md-6 col-lg-6 mb-4 d-flex justify-content-center">
-            <Card className="w-75 h-100 shadow mt-3 mb-4">
-              <Card.Img
-                variant="top"
-                src="https://hips.hearstapps.com/es.h-cdn.co/crfes/images/ninos/ocio/peliculas-para-ver-en-familia/brave-indomable/3633391-1-esl-ES/Brave-Indomable.jpg?resize=980:*"
-                className="foto"
-                
-              />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="danger" className="mt-2 shadow">Eliminar</Button>
-              </Card.Body>
-            </Card>
-          </div>
+   <div className="col-12 col-md-6 col-lg-6 mb-4 d-flex justify-content-center">
+      <Card className="w-75 h-100 shadow mt-3 mb-4">
+        <Card.Img
+          variant="top"
+          src="https://hips.hearstapps.com/es.h-cdn.co/crfes/images/ninos/ocio/peliculas-para-ver-en-familia/brave-indomable/3633391-1-esl-ES/Brave-Indomable.jpg?resize=980:*"
+          className="foto"
           
-          <div className="col-12 col-md-6 col-lg-6 mb-4 d-flex justify-content-center">
-            <Card className="w-75 h-100 shadow mt-3 mb-4">
-              <Card.Img
-                variant="top"
-                className="foto"
-                src="https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2024/06/14/17183742816234.jpg"
-                
-              />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="danger" className="mt-2 shadow">Eliminar</Button>
-              </Card.Body>
-            </Card>
-          </div>
-        </div>
-      </article>
-    </section>
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up
+            the bulk of the card's content.
+          </Card.Text>
+          <Button variant="danger" className="mt-2 shadow">Eliminar</Button>
+        </Card.Body>
+      </Card>
+    </div>
   );
 };
 

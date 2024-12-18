ddf154ff13fb58268975699030170116db8c87a6
Matias <matiassanchez13422@gmail.com>
2024-12-17 22:54:10 -0300

    Se corrigio algunas cosas de las cards

--- src/components/Peliculas.jsx
+++ src/components/Peliculas.jsx
@@ -5,13 +5,13 @@ import "../css/style.css"; // Si tienes estilos personalizados
 const PeliculasAgregadas = () => {
   return (
     <section>
-      <article>
-        <div>
+      <article className="fondo shadow" >
+        <div className="bg-white">
           <h2 className="text-center border p-2">Peliculas agregadas</h2>
         </div>
         <div className=" row justify-content-center">
           <div className="col-12 col-md-6 col-lg-6 mb-4 d-flex justify-content-center">
-            <Card className="w-75 h-100">
+            <Card className="w-75 h-100 shadow mt-3 mb-4">
               <Card.Img
                 variant="top"
                 src="https://hips.hearstapps.com/es.h-cdn.co/crfes/images/ninos/ocio/peliculas-para-ver-en-familia/brave-indomable/3633391-1-esl-ES/Brave-Indomable.jpg?resize=980:*"
@@ -24,13 +24,13 @@ const PeliculasAgregadas = () => {
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </Card.Text>
-                <Button variant="danger" className="mt-2">Eliminar</Button>
+                <Button variant="danger" className="mt-2 shadow">Eliminar</Button>
               </Card.Body>
             </Card>
           </div>
           
           <div className="col-12 col-md-6 col-lg-6 mb-4 d-flex justify-content-center">
-            <Card className="w-75 h-100">
+            <Card className="w-75 h-100 shadow mt-3 mb-4">
               <Card.Img
                 variant="top"
                 className="foto"
@@ -43,7 +43,7 @@ const PeliculasAgregadas = () => {
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </Card.Text>
-                <Button variant="danger" className="mt-2">Eliminar</Button>
+                <Button variant="danger" className="mt-2 shadow">Eliminar</Button>
               </Card.Body>
             </Card>
           </div>
